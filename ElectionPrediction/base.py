73e6e93de5134a548644bc99150f20a3904e8a2d
Lucas Cabral <lucas.cabral@ccc.ufcg.edu.br>
2018-10-17 11:27:18 -0300

    lab 3

--- ElectionPrediction/base.py
+++ ElectionPrediction/base.py
@@ -1,8 +1,33 @@
-# -*- coding: utf-8 -*-
-"""
-Spyder Editor
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib
 
-Este é um arquivo de script temporário.
-"""
+import matplotlib.pyplot as plt
+from scipy.stats import skew
+from scipy.stats.stats import pearsonr
 
-print("Hello World!")
+train = pd.read_csv("eleicoes_2006_a_2010.csv")
+test = pd.read_csv("eleicoes_2014.csv")
+
+train.head()
+
+all_data = pd.concat((train.loc[:,'ano':'descricao_ocupacao'],
+                      test.loc[:,'ano':'descricao_ocupacao']))
+
+matplotlib.rcParams['figure.figsize'] = (17.0, 5.0)
+prices = pd.DataFrame({"total_votos":train["votos"], "log(votos + 1)":np.log1p(train["votos"])})
+prices.hist(log=True)
+
+
+#log transform the target:
+train["votos"] = np.log1p(train["votos"])
+
+#log transform skewed numeric features:
+numeric_feats = all_data.dtypes[all_data.dtypes != "object"].index
+
+skewed_feats = train[numeric_feats].apply(lambda x: skew(x.dropna())) #compute skewness
+skewed_feats = skewed_feats[skewed_feats > 0.75]
+skewed_feats = skewed_feats.index
+
+all_data[skewed_feats] = np.log1p(all_data[skewed_feats])
